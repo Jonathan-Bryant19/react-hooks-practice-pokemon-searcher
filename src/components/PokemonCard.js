d39474c94087b6b1852dd85f5329716484d5a794
Jonathan Bryant <spectrum19tech@gmail.com>
2022-02-09 20:27:24 -0800

    Fetched data and added card click

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard( { name, front, back, hp } ) {
+  const [isFront, setIsFront] = useState(true)
+  
+  function handleOnClick() {
+    setIsFront(!isFront)
+  }
+  
+  
   return (
     <Card>
       <div>
         <div className="image">
-          <img alt="oh no!" />
+          {isFront 
+            ? <img src={front} alt="oh no!" onClick={handleOnClick}/>
+            : <img src={back} alt="oh no!" onClick={handleOnClick}/>
+          }
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {hp}
           </span>
         </div>
       </div>
